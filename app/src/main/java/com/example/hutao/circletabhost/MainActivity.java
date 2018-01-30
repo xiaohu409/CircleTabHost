9055e8eb7b77d0e049992186108c17784575083a
hutao <285279904@qq.com>
2018-01-30 23:33:21 +0800

    添加注释

--- app/src/main/java/com/example/hutao/circletabhost/MainActivity.java
+++ app/src/main/java/com/example/hutao/circletabhost/MainActivity.java
@@ -8,10 +8,9 @@ import com.example.hutao.circletabhost.fragment.BlankFragment2;
 import com.example.hutao.circletabhost.fragment.BlankFragment3;
 import com.example.hutao.circletabhost.fragment.BlankFragment4;
 import com.example.hutao.circletabhost.fragment.BlankFragment5;
-import com.example.hutao.circletabhost.util.ToastUtil;
 import com.hutao.circletabhost.widget.CircleTabHost;
 
-public class MainActivity extends AppCompatActivity implements CircleTabHost.OnTabListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,30 +20,35 @@ public class MainActivity extends AppCompatActivity implements CircleTabHost.OnT
     }
 
     private void intiUI() {
+        //图标
         int[] tabIco = {R.drawable.home_sel, R.drawable.tfaccount_sel,
                 R.drawable.discover_sel, R.drawable.cart_sel,
                 R.drawable.account_sel};
-
+        //文本
         String[] tabTitle = getResources().getStringArray(R.array.tab_title);
-
+        //Fragment
         Class<?>[] tabFragment = {BlankFragment1.class, BlankFragment2.class,
                 BlankFragment3.class, BlankFragment4.class, BlankFragment5.class};
-
+        //实例化TabHost
         CircleTabHost circleTabHost = findViewById(R.id.circle_tab_id);
-
         for (int i = 0; i < tabTitle.length; i++) {
+            //实例化Tab
             CircleTabHost.TabParam tabParam = new CircleTabHost.TabParam(tabIco[i], tabTitle[i], tabFragment[i]);
+            //添加Tab
             circleTabHost.addTab(tabParam);
         }
-
+        //配置FragmentManager和Content id
         circleTabHost.setup(getSupportFragmentManager(), R.id.content_id);
-        circleTabHost.setOnTabListener(this);
+        //回调方法
+        circleTabHost.setOnTabListener(new CircleTabHost.OnTabListener() {
+            @Override
+            public void onTabChange(int index, CircleTabHost.TabParam param) {
+
+            }
+        });
+        //设置角标
         circleTabHost.setCornerMark(2, "11");
         circleTabHost.setCornerMark(3, "8");
     }
 
-    @Override
-    public void onTabChange(int index, CircleTabHost.TabParam param) {
-        ToastUtil.showShort(this, "index:" + index);
-    }
 }
